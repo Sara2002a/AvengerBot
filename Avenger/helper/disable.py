49090910b2719f49377a5c38250d2ca54aba123b
Team Avenger <103058442+TeamAvengerBot@users.noreply.github.com>
2022-04-06 07:40:33 +0530

    Create disable.py

---
+++ Avenger/helper/disable.py
@@ -0,0 +1,28 @@
+from Avenger.database.disable_mongo import get_disabled, get_disabledel
+from Avenger.helper.chat_status import isBotCan, isUserAdmin
+from Avenger.helper.custom_filter import DISABLE_COMMANDS
+
+
+def disable(func):
+
+    async def wrapper(client, message):
+        if not message.command:
+            return
+        if not await isUserAdmin(message, silent=True):
+            chat_id = message.chat.id
+            command = message.command[0]
+            DISABLED_LIST = get_disabled(chat_id)
+            if command in DISABLED_LIST:
+                if get_disabledel(chat_id):
+                    if not await isBotCan(message, permissions='can_delete_messages'):
+                        return
+                    await message.delete()
+                    return
+                else:
+                    return
+            else:
+                await func(client, message)
+        else:
+            await func(client, message)
+    
+    return wrapper
