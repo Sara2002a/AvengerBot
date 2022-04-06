d07c33b3814b926203c0be08386e2bd089d7b3e4
Team Avenger <103058442+TeamAvengerBot@users.noreply.github.com>
2022-04-06 10:36:20 +0530

    Create note_misc_helper.py

---
+++ Avenger/helper/note_helper/note_misc_helper.py
@@ -0,0 +1,42 @@
+from pyrogram.types import Message
+from Avenger.helper.chat_status import isUserAdmin
+
+async def privateNote_and_admin_checker(message, text: str):
+    privateNote = True
+    if '{noprivate}' in text:
+        privateNote = False 
+    elif '{private}' in text:
+        privateNote = True 
+    else:
+        privateNote = None
+
+    allow = True
+    if '{admin}' in text:
+        if not await isUserAdmin(message, silent=True):
+            allow = False 
+        else:
+            allow = True 
+    
+    return (
+        privateNote,
+        allow
+    )
+
+def preview_text_replace(text):
+        if '{preview}' in text:
+            text = text.replace('{preview}', '')
+            preview =  False
+        else:
+            preview = True
+        
+        if '{admin}' in text:
+            text = text.replace('{admin}', '')
+        
+        if '{private}' in text:
+            text = text.replace('{private}', '')
+        elif '{noprivate}' in text:
+            text = text.replace('{noprivate}', '')
+        
+        return (
+            preview, text
+        )
